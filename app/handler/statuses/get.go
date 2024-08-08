1ad329c11d342687d1dce7941e3b069d987d5597
shimizu <noon3m.op.sh@gmail.com>
2024-08-08 22:17:23 +0900

    [change]accountの構造体を渡すようにした

--- app/handler/statuses/get.go
+++ app/handler/statuses/get.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
+	"yatter-backend-go/app/domain/object"
 	"yatter-backend-go/app/usecase"
 )
 
@@ -33,33 +34,21 @@ func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
 
 // レスポンス用の構造体
 type HTTPResponse struct {
-	ID      int
-	Account struct {
-		ID        int
-		UserName  string
-		CreatedAt time.Time
-	}
+	ID        int
+	Account   object.Account
 	Content   string
 	CreatedAt time.Time
 }
 
 type GetStatus struct {
-	Statues *HTTPResponse
+	Statuses *HTTPResponse
 }
 
 func newResponse(dto *usecase.GetStatusDTO) *GetStatus {
 	return &GetStatus{
-		Statues: &HTTPResponse{
-			ID: dto.Status.ID,
-			Account: struct {
-				ID        int
-				UserName  string
-				CreatedAt time.Time
-			}{
-				ID:        int(dto.Account.ID),
-				UserName:  dto.Account.Username,
-				CreatedAt: dto.Account.CreateAt,
-			},
+		Statuses: &HTTPResponse{
+			ID:        dto.Status.ID,
+			Account:   *dto.Account,
 			Content:   dto.Status.Content,
 			CreatedAt: dto.Status.CreatedAt,
 		},
